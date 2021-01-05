392da7a58780d1ea548cf7d7d1d292f263219909
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2021-01-05 23:41:27 +0300

    Create XOgameApp.swift

---
+++ XOgame/XOgame/XOgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOgameApp.swift
+//  XOgame
+//
+//  Created by Mariam2 on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct XOgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
